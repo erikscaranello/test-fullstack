41cd7b2a073b3bb7c549538ac1f6aa85554d2e88
Erik Scaranello <scaranelloerik@yahoo.it>
2017-10-23 22:40:33 -0200

    resolucao do teste

--- test-ui/src/main/resources/static/app/app-route.js
+++ test-ui/src/main/resources/static/app/app-route.js
@@ -7,6 +7,20 @@ angular.module('test').config(['$stateProvider', function ($stateProvider) {
 		url: '/home',
 		templateUrl: '/app/app-template.html',
 		controller: 'AppController'
+	
+	}).state('listaUsuarios', {
+		url: '/user',
+		templateUrl: '/app/views/listaUsuario.html',
+		controller: 'ListaUsuarioController'
+	
+	}).state('adicionaUsuario', {
+		url: '/user/add',
+		templateUrl: '/app/views/adicionaUsuario.html',
+		controller: 'AdicionaUsuarioController'
+	
+	}).state('updateUsuario', {
+		url: '/user/update/:id',
+		templateUrl: '/app/views/adicionaUsuario.html',
+		controller: 'UpdateUsuarioController'
 	});
-
 }]);
